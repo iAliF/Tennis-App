85b5b46ba9299dff8802dfed383dfc687ae53040
iAliF <mr.ali.fotouhi@gmail.com>
2023-03-21 05:56:31 +0330

    Add new player dialog

--- app/src/main/java/ir/thealif/tennis/MainActivity.kt
+++ app/src/main/java/ir/thealif/tennis/MainActivity.kt
@@ -1,6 +1,8 @@
 package ir.thealif.tennis
 
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import ir.thealif.tennis.databinding.ActivityMainBinding
@@ -8,6 +10,7 @@ import ir.thealif.tennis.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var myAdapter: MyRecyclerViewAdapter
     private var dataModelList: ArrayList<DataModel> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +22,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-        val adapter = MyRecyclerViewAdapter(this, dataModelList)
-        binding.recyclerViewAdapter = adapter
+        myAdapter = MyRecyclerViewAdapter(this, dataModelList)
+        binding.recyclerViewAdapter = myAdapter
+        binding.fabAddPlayer.setOnClickListener {
+            showAddDialog()
+        }
+    }
+
+    private fun showAddDialog() {
+        val input = EditText(this)
+
+        AlertDialog.Builder(this)
+            .setTitle("Add new player")
+            .setView(input)
+            .setPositiveButton("Add") { _, _ ->
+                dataModelList.add(DataModel(input.text.toString()))
+                myAdapter.notifyItemInserted(dataModelList.size)
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.cancel()
+            }
+            .show()
     }
 
     private fun loadData() {
