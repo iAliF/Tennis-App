a784668765baab953ea72e1d3d9c901de5c9e7c7
iAliF <mr.ali.fotouhi@gmail.com>
2023-03-22 05:23:58 +0430

    Fix

--- app/src/main/java/ir/thealif/tennis/models/PlayerModel.kt
+++ app/src/main/java/ir/thealif/tennis/models/PlayerModel.kt
@@ -1,3 +1,18 @@
 package ir.thealif.tennis.models
 
-data class PlayerModel(val name: String)
+import java.util.*
+
+data class PlayerModel(val name: String) {
+    var uuid: UUID = UUID.randomUUID()
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is PlayerModel)
+            return false
+
+        return this.uuid == other.uuid
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode() * uuid.hashCode()
+    }
+}
