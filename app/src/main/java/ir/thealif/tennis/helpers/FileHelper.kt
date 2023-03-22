785142c127e1be502ca04082b9cb50408c306e96
iAliF <mr.ali.fotouhi@gmail.com>
2023-03-22 18:17:49 +0430

    Bug Fix

--- app/src/main/java/ir/thealif/tennis/helpers/FileHelper.kt
+++ app/src/main/java/ir/thealif/tennis/helpers/FileHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import ir.thealif.tennis.models.PlayerModel
 import org.json.JSONArray
 import java.io.BufferedReader
+import java.io.FileNotFoundException
 import java.io.InputStreamReader
 
 class FileHelper(private val context: Context) {
@@ -45,24 +46,28 @@ class FileHelper(private val context: Context) {
         fileOutStream.close()
     }
 
-    private fun readFile(): String {
-        val fileInStream = context.openFileInput(FILENAME)
-        val inStreamReader = InputStreamReader(fileInStream)
-        val bufferedReader = BufferedReader(inStreamReader)
-        val stringBuilder = StringBuilder()
-        var text: String?
-        while (run {
-                text = bufferedReader.readLine()
-                text
-            } != null) {
-            stringBuilder.append(text)
-        }
+    private fun readFile(): String? {
+        try {
+            val fileInStream = context.openFileInput(FILENAME)
+            val inStreamReader = InputStreamReader(fileInStream)
+            val bufferedReader = BufferedReader(inStreamReader)
+            val stringBuilder = StringBuilder()
+            var text: String?
+            while (run {
+                    text = bufferedReader.readLine()
+                    text
+                } != null) {
+                stringBuilder.append(text)
+            }
 
-        return stringBuilder.toString()
+            return stringBuilder.toString()
+        } catch (err: FileNotFoundException) {
+            return null
+        }
     }
 
     fun loadData(): ArrayList<PlayerModel> {
-        val fileData = readFile()
+        val fileData = readFile() ?: return ArrayList()
         val jsonArray = JSONArray(fileData)
         return convertJsonArray(jsonArray)
     }
